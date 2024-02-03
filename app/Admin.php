a4dc72e1e9e77c56f1321f3cdacc1d26466dc662
JudyHopps007 <153383542+JudyHopps007@users.noreply.github.com>
2024-02-03 17:45:11 +0900

    Update Admin.php

--- app/Admin.php
+++ app/Admin.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Admin extends Authenticatable
 {
     use Notifiable;
-
+    
     /**
      * The attributes that are mass assignable.
      *
